fdc833d4482dc1b923ca3c5f9cd1f710d96848a4
ulrich aguey <ulrichatlanta@hotmail.com>
2024-01-16 19:24:50 +0100

    the end

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,9 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /restaurants or /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @articles = Article.all
   end
 
   # GET /restaurants/1 or /restaurants/1.json
@@ -12,7 +12,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/new
   def new
-    @restaurant = Restaurant.new
+    @article = Article.new
   end
 
   # GET /restaurants/1/edit
@@ -21,15 +21,15 @@ class RestaurantsController < ApplicationController
 
   # POST /restaurants or /restaurants.json
   def create
-    @restaurant = Restaurant.new(restaurant_params)
+    @article = Article.new(article_params)
 
     respond_to do |format|
-      if @restaurant.save
-        format.html { redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully created." }
-        format.json { render :show, status: :created, location: @restaurant }
+      if @article.save
+        format.html { redirect_to restaurant_url(@article), notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -37,34 +37,34 @@ class RestaurantsController < ApplicationController
   # PATCH/PUT /restaurants/1 or /restaurants/1.json
   def update
     respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully updated." }
-        format.json { render :show, status: :ok, location: @restaurant }
+      if @article.update(article_params)
+        format.html { redirect_to restaurant_url(@article), notice: "Article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /restaurants/1 or /restaurants/1.json
   def destroy
-    @restaurant.destroy
+    @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
+      format.html { redirect_to articles_url, notice: "Restaurant was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
+    def set_article
+      @article = Article.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :rating)
+    def article_params
+      params.require(:article).permit(:title, :content)
     end
 end
